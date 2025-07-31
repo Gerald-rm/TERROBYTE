5b6b99b5906b82af6038021e1dd3e222a0008bf3
Karro-lab <tablemousekar@gmail.com>
2025-07-30 18:19:03 -0700

    Apuesta millonaria

---
+++ estatus.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class estatus{
+    public static void main(String[] args) {
+        //estatuses a consultar: Terminado, En progreso, Pendiente, Cancelado.
+        System.out.println("");
+    }
+}
