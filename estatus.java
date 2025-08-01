a7fa52d424ddab2e75b02aa2ca38b25ec6ebb458
Karro-lab <tablemousekar@gmail.com>
2025-08-01 09:04:05 -0700

    Comentario modificado
    lol

--- estatus.java
+++ estatus.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class estatus{
     public static void main(String[] args) {
-        //estatuses a consultar: Terminado, En progreso, Pendiente, Cancelado.
+        //estatuses a consultar: Terminado, En progreso, Pendiente, Cancelado. Ahora david lo hara lol
         System.out.println("");
     }
 }
