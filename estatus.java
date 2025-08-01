b415d5e2b35955aaa7edd5c12ad6d9f657e037b8
Karro-lab <tablemousekar@gmail.com>
2025-08-01 09:19:16 -0700

    Comentario actualizacion pt2
    probando el pull request otra vez cd

--- estatus.java
+++ estatus.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class estatus{
     public static void main(String[] args) {
-        //estatuses a consultar: Terminado, En progreso, Pendiente, Cancelado. Ahora david lo hara lol
+        //estatuses a consultar: Terminado, En progreso, Pendiente, Cancelado. Ahora david lo hara, supongo lol
         System.out.println("");
     }
 }
